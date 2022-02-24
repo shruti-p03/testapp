f8123fe79e49566f14e614efe93f8ef952bd987f
AlexyPulivelil <alexypulivelil@gmail.com>
2022-02-24 18:08:14 +0530

    pipeline test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
     // Respond with "hello world" for requests that hit our root "/"
     app.get("/", function (req, res) {
-     return res.send("Hello World!!!!");
+     return res.send("Hello World12");
     });
 
     // listen to port 7000 by default
